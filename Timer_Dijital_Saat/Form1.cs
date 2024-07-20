938f6a816414dff03e23e25574555e9ef4705791
yesimdemir <yesimdmr000@gmail.com>
2024-07-20 16:26:22 +0300

    Timer_Dijital_Saat

---
+++ Timer_Dijital_Saat/Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Timer_Dijital_Saat
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        int saat = 0, dakika = 0, saniye = 0;
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            saniye++;
+            label1.Text=saniye.ToString();
+            if (saniye == 60)
+            {
+                dakika++;
+                label2.Text=dakika.ToString();
+                saniye = 0;
+                if(dakika== 60)
+                {
+                    saat = saat + 1;
+                    label3.Text=saat.ToString();
+                    dakika = 0;
+
+
+                }
+
+            }
+        }
+    }
+}
